81f7dc217e1351329b06707b436bb67fa6a50c73
Lisica797 <kristina_maisak@mail.ru>
2021-10-12 01:00:45 +0300

    + Jenkinsfile_declarative.groovy

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 import static utils.PropConst.*;
 
@@ -41,7 +46,7 @@ public class DriverManager {
 
     private void initDriver() {
         if (OS.isFamilyWindows()) {
-            initDriverWindowsOsFamily();
+            initDriverAnyOsFamily1();
         }
     }
 
@@ -64,6 +69,35 @@ public class DriverManager {
                 break;
             default:
                 Assertions.fail("Типа браузера '" + props.getProperty(TYPE_BROWSER) + "' не существует во фреймворке");
+        }
+    }
+
+    /**
+     * Метод инициализирующий веб драйвер
+     */
+    private void initDriverAnyOsFamily1() {
+        switch (props.getProperty(TYPE_BROWSER)) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", props.getProperty(PATH_GECKO_DRIVER_WINDOWS));
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", props.getProperty(PATH_CHROME_DRIVER_WINDOWS));
+                driver = new ChromeDriver();
+                break;
+            case "remote":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("80.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+                try {
+                    driver = new RemoteWebDriver(
+                            URI.create("http://161.35.194.216:4444/wd/hub").toURL(),
+                            capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
         }
     }
 }
